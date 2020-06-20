3c5a8e588e85d59f98f2111b84391af6d0e20b12
ERAZERX6603\Account <denistaskinov@gmail.com>
2020-06-20 12:08:36 +0300

    Drugos

--- Assets/Scripts/CardManagerScr.cs
+++ Assets/Scripts/CardManagerScr.cs
@@ -162,39 +162,39 @@ public class CardManagerScr : MonoBehaviour
 {
     public void Awake()
     {
-        CardManager.AllCards.Add(new Card("ebalo", "Sprites/Cards/ebalo", 5, 5, 6));
-        CardManager.AllCards.Add(new Card("buldiga", "Sprites/Cards/buldiga", 4, 3, 5));
-        CardManager.AllCards.Add(new Card("hmm", "Sprites/Cards/hmm", 3, 3, 4));
-        CardManager.AllCards.Add(new Card("micro", "Sprites/Cards/micro", 2, 1, 2));
-        CardManager.AllCards.Add(new Card("pominki", "Sprites/Cards/pominki", 8, 1, 7));
-        CardManager.AllCards.Add(new Card("pomoika", "Sprites/Cards/pomoika", 1, 1, 1));
+        CardManager.AllCards.Add(new Card("Necromancer", "Sprites/Cards/necromancer", 5, 5, 6));
+        CardManager.AllCards.Add(new Card("Angel", "Sprites/Cards/angel", 4, 3, 5));
+        CardManager.AllCards.Add(new Card("Magician", "Sprites/Cards/magician", 3, 3, 4));
+        CardManager.AllCards.Add(new Card("Assassins", "Sprites/Cards/assassins", 2, 1, 2));
+        CardManager.AllCards.Add(new Card("Barbarian", "Sprites/Cards/barbarian", 8, 1, 7));
+        CardManager.AllCards.Add(new Card("Stalker", "Sprites/Cards/stalker", 1, 1, 1));
 
-        CardManager.AllCards.Add(new Card("provocation", "Sprites/Cards/provocation", 1, 2, 3, Card.AbilityType.PROVOCATION));
-        CardManager.AllCards.Add(new Card("regeneration", "Sprites/Cards/regen", 4, 2, 5, Card.AbilityType.REGENERATION_EACH_TURN));
-        CardManager.AllCards.Add(new Card("doubleAttack", "Sprites/Cards/doubleAttack", 3, 2, 4, Card.AbilityType.DOUBLE_ATTACK));
-        CardManager.AllCards.Add(new Card("instantActive", "Sprites/Cards/instantActive", 2, 1, 2, Card.AbilityType.INSTANT_ACTIVE));
-        CardManager.AllCards.Add(new Card("shield", "Sprites/Cards/shield", 5, 1, 7, Card.AbilityType.SHIELD));
-        CardManager.AllCards.Add(new Card("counterAttack", "Sprites/Cards/counterAttack", 3, 1, 1, Card.AbilityType.COUNTER_ATTACK));
+        CardManager.AllCards.Add(new Card("Infantryman provocation", "Sprites/Cards/provocation", 1, 2, 3, Card.AbilityType.PROVOCATION));
+        CardManager.AllCards.Add(new Card("Сleric regeneration", "Sprites/Cards/regen", 4, 2, 5, Card.AbilityType.REGENERATION_EACH_TURN));
+        CardManager.AllCards.Add(new Card("Lich King doubleAttack", "Sprites/Cards/doubleAttack", 3, 2, 4, Card.AbilityType.DOUBLE_ATTACK));
+        CardManager.AllCards.Add(new Card("Wolf hunter instantActive", "Sprites/Cards/instantActive", 2, 1, 2, Card.AbilityType.INSTANT_ACTIVE));
+        CardManager.AllCards.Add(new Card("Demon shield", "Sprites/Cards/shield", 5, 1, 7, Card.AbilityType.SHIELD));
+        CardManager.AllCards.Add(new Card("Heroes counterAttack", "Sprites/Cards/counterAttack", 3, 1, 1, Card.AbilityType.COUNTER_ATTACK));
 
-        CardManager.AllCards.Add(new SpellCard("HEAL_ALLY_FIELD_CARDS", "Sprites/Cards/healAllyCards", 2,
+        CardManager.AllCards.Add(new SpellCard("SPELLS HEAL_ALLY_FIELD_CARDS", "Sprites/Cards/healAllyFieldCards", 2,
             SpellCard.SpellType.HEAL_ALLY_FIELD_CARDS, 2, SpellCard.TargetType.NO_TARGET));
-        CardManager.AllCards.Add(new SpellCard("DAMAGE_ENEMY_FIELD_CARDS", "Sprites/Cards/damageEnemyCards", 2,
+        CardManager.AllCards.Add(new SpellCard("SPELLS DAMAGE_ENEMY_FIELD_CARDS", "Sprites/Cards/damageEnemyCards", 2,
             SpellCard.SpellType.DAMAGE_ENEMY_FIELD_CARDS, 2, SpellCard.TargetType.NO_TARGET));
-        CardManager.AllCards.Add(new SpellCard("HEAL_ALLY_HERO", "Sprites/Cards/healAllyHero", 2,
+        CardManager.AllCards.Add(new SpellCard("SPELLS HEAL_ALLY_HERO", "Sprites/Cards/healAllyHero", 2,
             SpellCard.SpellType.HEAL_ALLY_HERO, 2, SpellCard.TargetType.NO_TARGET));
-        CardManager.AllCards.Add(new SpellCard("DAMAGE_ENEMY_HERO", "Sprites/Cards/damageEnemyHero", 2,
+        CardManager.AllCards.Add(new SpellCard("SPELLS DAMAGE_ENEMY_HERO", "Sprites/Cards/damageEnemyHero", 2,
             SpellCard.SpellType.DAMAGE_ENEMY_HERO, 2, SpellCard.TargetType.NO_TARGET));
-        CardManager.AllCards.Add(new SpellCard("HEAL_ALLY_CARD", "Sprites/Cards/healAllyCard", 2,
+        CardManager.AllCards.Add(new SpellCard("SPELLS HEAL_ALLY_CARD", "Sprites/Cards/healAllyCard", 2,
             SpellCard.SpellType.HEAL_ALLY_CARD, 2, SpellCard.TargetType.ALLY_CARD_TARGET));
-        CardManager.AllCards.Add(new SpellCard("DAMAGE_ENEMY_CARD", "Sprites/Cards/damageEnemyCard", 2,
+        CardManager.AllCards.Add(new SpellCard("SPELLS DAMAGE_ENEMY_CARD", "Sprites/Cards/damageEnemyCard", 2,
             SpellCard.SpellType.DAMAGE_ENEMY_CARD, 2, SpellCard.TargetType.ENEMY_CARD_TARGET));
-        CardManager.AllCards.Add(new SpellCard("SHIELD_ON_ALLY_CARD", "Sprites/Cards/shieldOnAllyCard", 2,
+        CardManager.AllCards.Add(new SpellCard("SPELLS SHIELD_ON_ALLY_CARD", "Sprites/Cards/shieldOnAllyCard", 2,
             SpellCard.SpellType.SHIELD_ON_ALLY_CARD, 0, SpellCard.TargetType.ALLY_CARD_TARGET));
-        CardManager.AllCards.Add(new SpellCard("PROVOCATION_ON_ALLY_CARD", "Sprites/Cards/provocationOnAllyCard", 2,
+        CardManager.AllCards.Add(new SpellCard("SPELLS PROVOCATION_ON_ALLY_CARD", "Sprites/Cards/provocationOnAllyCard", 2,
             SpellCard.SpellType.PROVOCATION_ON_ALLY_CARD, 0, SpellCard.TargetType.ALLY_CARD_TARGET));
-        CardManager.AllCards.Add(new SpellCard("BUFF_CARD_DAMAGE", "Sprites/Cards/buffCardDamage", 2,
+        CardManager.AllCards.Add(new SpellCard("SPELLS BUFF_CARD_DAMAGE", "Sprites/Cards/buffCardDamage", 2,
             SpellCard.SpellType.BUFF_CARD_DAMAGE, 2, SpellCard.TargetType.ALLY_CARD_TARGET));
-        CardManager.AllCards.Add(new SpellCard("DEBUFF_CARD_DAMAGE", "Sprites/Cards/debuffCardDamage", 2,
+        CardManager.AllCards.Add(new SpellCard("SPELLS DEBUFF_CARD_DAMAGE", "Sprites/Cards/debuffCardDamage", 2,
             SpellCard.SpellType.DEBUFF_CARD_DAMAGE, 2, SpellCard.TargetType.ENEMY_CARD_TARGET));
     }
 }
